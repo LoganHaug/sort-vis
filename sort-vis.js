2ddb59b1044a95141b84592f9efab586070e34c1
Logan Haug <loganhaug@Logans-MacBook-Pro.local>
2020-03-21 23:51:40 -0700

    Starter code for block class, and blocks on screen

--- sort-vis.js
+++ sort-vis.js
@@ -1,3 +1,24 @@
+// Makes the class block an object of which is the blocks display
+class block {
+  // Takes a width, height, and a p5 vector
+  constructor(width, height, vect){
+    this.width = width;
+    this.height = height;
+    this.vect = vect
+  }
+  draw_block() {
+    fill(randint(0,255), randint(0,255), randint(0,255));
+    rect(this.vect.x, this.vect.y, this.width, this.height);
+  }
+}
+
+function randint(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+
+
+
 var cnv;
 
 // Centers the canvas
@@ -10,7 +31,22 @@ function centerCanvas() {
 function setup() {
   cnv = createCanvas(1000, 600);
   centerCanvas();
-  background(200, 0, 255);
+  background(255, 255, 255);
+
+
+  var blocks = [];
+  var num_blocks = 10;
+  for (var i = 0; i < num_blocks; i++){
+    append(blocks,
+      new block(10,
+      randint(-15,-200),
+      createVector(i * 100, 300)
+    ));
+  }
+  for (var i in blocks){
+    blocks[i].draw_block();
+  }
+
 }
 // Re-centers the canvas when the window is resized
 function windowResized() {
